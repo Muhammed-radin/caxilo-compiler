6909a635973cc43c150731737f25955f599bd966
muhammed-radin <rasiyakaratt@gmail.com>
2022-01-28 13:19:43 +0530

    liverun added

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 console.log($("textarea").val());
-function run(){
+
+function run() {
   Msg('running')
   $("#res").append($("textarea").val())
   document.getElementById('runBtn').disabled = true
   $("#runBtn").html(`<i class="fa fa-play"></i><l></l>running`)
 }
-function stop(){
+
+function stop() {
   $("#res").remove()
   $("html").append(`<div id="res"></div>`)
-  document.getElementById('runBtn').disabled = false 
+  document.getElementById('runBtn').disabled = false
   $("#runBtn").html(`<i class="fa fa-play"></i><l></l>run`)
+}
+
+function liverun() {
+  document.getElementById('runBtn').disabled = true
+  document.getElementById('stopBtn').disabled = true
+  $("#liveBtn").html(`<i class="fa fa-headset"></i><l></l>live running`)
+  setInterval(function() {
+    $("#res").remove()
+    $("html").append(`<div id="res"></div>`)
+    $("#res").append($("textarea").val())
+  }, 500)
 }
