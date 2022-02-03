cc34985366d8482295f28657b0b6c86ca3f4882a
muhammed-radin <rasiyakaratt@gmail.com>
2022-02-03 12:56:01 +0530

    version 2.0 second commit [ run stop open and save ]

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 
  var editor = ace.edit("editor");
     editor.setTheme("ace/theme/chrome");
-    editor.session.setMode("ace/mode/");
-editor.setValue('<html><html>')
+    editor.session.setMode("ace/mode/html");
+editor.setValue('')
 
 function run() {
   $("#res").remove()
   $("html").append(`<div id="res"></div>`)
   Msg('running')
-  $("#res").append($("textarea").val())
+  $("#res").append(editor.getValue())
   document.getElementById('runBtn').disabled = true
-  $("#runBtn").html(`<i class="fa fa-play"></i><l></l>running`)
+ // $("#runBtn").html(`<i class="fa fa-play"></i><l></l>running`)
+}
+function pass(){
+    onHider('editor',true)
+    run()
 }
 
 function stop() {
+  onHider('res',true)
+  openModal('editor')
   $("#res").remove()
   $("html").append(`<div id="res"></div>`)
   document.getElementById('runBtn').disabled = false
-  $("#runBtn").html(`<i class="fa fa-play"></i><l></l>run`)
+  //$("#runBtn").html(`<i class="fa fa-play"></i><l></l>run`)
 }
 
 function liverun() {
@@ -28,13 +34,13 @@ function liverun() {
   setInterval(function() {
     $("#res").remove()
     $("html").append(`<div id="res"></div>`)
-    $("#res").append($("textarea").val())
+    $("#res").append(editor.getValue())
   }, 500)
 }
 
 function saveDoc() {
   onHider('falert', true)
-  localStorage.setItem($("#key").val(), $("textarea").val())
+  localStorage.setItem($("#key").val(), editor.getValue())
   Msg('saved..')
 }
 
@@ -44,7 +50,8 @@ function openDoc() {
   if (key ? true : false) {
     if(localStorage.getItem(key) ? true : false){
     var vale = localStorage.getItem(key)
-    document.querySelector('textarea').innerHTML = vale
+    //document.querySelector('textarea').innerHTML = vale
+    editor.setValue(vale)
     Msg('opened...')
     }else {
       alertNot(`not found key:can't open`,'var(--bg)','open','openModal(`falerta`)','fa fa-box','5s','blue')
@@ -58,4 +65,25 @@ function openModal(id) {
   $("body").append(`<style></style>`)
   $(id).css('display', 'block')
   document.getElementById(id).style.display = 'block'
+}
+var date = new Date()
+
+function sumWin(){
+  $("html").append(`<div id="res"></div>`)
+  $("style").append(`#editor {
+    height: 45vh;
+    display: block;
+  }
+  #res {
+    display: block;
+    top: 50vh !important;
+  }`)
+  liverun()
+}
+function rBtn(){
+  alert()
+  $("#res").remove()
+  $("html").append(`<div id="res"></div>`)
+  Msg('running')
+  $("#res").append(editor.getValue())
 }
